a121bd0ed9dd302c5ea890387a18040e40624f9f
yasmin3A <00001105948584sp@al.educacao.sp.gov.br>
2024-11-21 17:13:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é o país mais populoso do mundo?',
+    'India'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é a capital da Inglaterra?',
+    'A capital da inglaterra é londres'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu te amo em Inglês?',
+    'Eu te amo em ingles é I love you'
+)
